917b2191569137b4eb2ae0ab41a3d9fc6d87c53b
Sophie Lian <sophie2220@berkeley.edu>
2020-05-08 02:04:07 -0700

    fully integrated with design

--- Front_End/background.js
+++ Front_End/background.js
@@ -1,15 +1,52 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        /* 
+        What needs to be changes here is: 
+        - Use the URL/domain as the key for the API get 
+          function to get the grade from the database. 
+        - Then use the grading getted from the database in 
+          the if-statement so that the right bulk of code 
+          is triggered for the right grad. Right now it’s 
+          hard coded and should be replaced with the grad.
+        */
+        const Http = new XMLHttpRequest();
+        Http.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+                console.log(Http.responseText)
+            }
+        }
+        Http.open("GET", "http://localhost:5000/policy");
+        Http.send();
+
         var activeTab = tabs[0];
         var url = activeTab.url;
-        if (msg.action === "updateIcon" && url.includes('discord')) {
+        if (url.includes('facebook')) {
             chrome.browserAction.setIcon({
-                path: "/img/green.png"
+                path: {
+                    "32": "/img/red.png",
+                    "16": "/img/red.png"
+                }
             });
-        }
-        if (msg.action === "updateIcon" && url.includes('reddit')) {
+        } else if (url.includes('google')) {
             chrome.browserAction.setIcon({
-                path: "/img/gray.png" 
+                path: {
+                    "32": "/img/yellow.png",
+                    "16": "/img/yellow.png"
+                }
+            });
+        } else if (url.includes('figma')) {
+            chrome.browserAction.setIcon({
+                path: {
+                    "32": "/img/green.png",
+                    "16": "/img/green.png"
+                }
+            });
+        } else {
+            chrome.browserAction.setIcon({
+                path: {
+                    "32": "/img/blue.png",
+                    "16": "/img/blue.png"
+                }
             });
         }
     });
--- Front_End/popup.js
+++ Front_End/popup.js
@@ -1,13 +1,104 @@
 $(function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        /* 
+        What needs to be changes here is: 
+        - Use the URL/domain as the key for the API get 
+          function to get the grade from the database. 
+        - Then use the grading getted from the database in 
+          the if-statement so that the right bulk of code 
+          is triggered for the right grad. Right now it’s 
+          hard coded with the lists w, m, b - this should 
+          replaced with the grad from the database.
+        */
+        const Http = new XMLHttpRequest();
+        Http.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(Http.responseText)
+            }
+        }
+        Http.open("GET", "http://localhost:5000/policy");
+        Http.send();
+
+        var w = ["facebook"];
+        var m = ["google"];
+        var b = ["figma"];
         var activeTab = tabs[0];
-        var url = activeTab.url;
-        var file = "/img/gray.png";
-        if (url.includes('discord')) {
-            file = "/img/better.png";
-        } else if (url.includes('reddit')) {
-            file = "/img/unknown.png";
-        } 
-        $('#rank').attr("src", file);
+        var url = activeTab.url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
+        var worse;
+        var moderate = "Unknown";
+        var better;
+        var icon = "/img/blue.png";
+        if (w.some(v => url.includes(v))) {
+            worse = "Worse";
+            moderate = "Moderate";
+            better = "Better";
+            icon = "/img/red.png";
+            $('#dot').css({
+                "position": "absolute",
+                "width": "7px",
+                "height": "7px",
+                "left": "75px",
+                "top": "124px",
+                "background": "#EB5757",
+                "border-radius": "50%"});
+            var obj = $('#text').text("Worse because it:\n - Don't keep your information private\n - Track your actions\n - Give you targeted ads");
+            obj.html(obj.html().replace(/\n/g,'<br/>'));
+        } else if (m.some(v => url.includes(v))) {
+            worse = "Worse";
+            moderate = "Moderate";
+            better = "Better";
+            icon = "/img/yellow.png";
+            $('#dot').css({
+                "position": "absolute",
+                "width": "7px",
+                "height": "7px",
+                "left": "168px",
+                "top": "124px",
+                "background": "#F2C94C",
+                "border-radius": "50%"});
+            var obj = $('#text').text("Moderate because it:\n - Keeps your information private\n - Track your actions\n - Doesn't give you targeted ads");
+            obj.html(obj.html().replace(/\n/g,'<br/>'));
+        } else if (b.some(v => url.includes(v))) {
+            worse = "Worse";
+            moderate = "Moderate";
+            better = "Better";
+            icon = "/img/green.png";
+            $('#dot').css({
+                "position": "absolute",
+                "width": "7px",
+                "height": "7px",
+                "left": "261px",
+                "top": "124px",
+                "background": "#219653",
+                "border-radius": "50%"});
+                var obj = $('#text').text("Better because it:\n - Keeps your information private\n - Doesn't track your actions\n - Doesn't give you targeted ads");
+                obj.html(obj.html().replace(/\n/g,'<br/>'));
+
+        } else {
+            icon = "/img/blue.png";
+            $('#dot').css({
+                "position": "absolute",
+                "width": "7px",
+                "height": "7px",
+                "left": "168px",
+                "top": "124px",
+                "background": "#2d9cdb",
+                "border-radius": "50%"});
+            $('#line').css({
+                "position": "absolute",
+                "width": "279px",
+                "height": "1px",
+                "left": "32px",
+                "top": "127px",
+                "background": "#2d9cdb",
+                "border-radius": "10px"});
+            var obj = $('#text').text("\nCategorized as unknown because we don't have enough data to categorize it.");
+            obj.html(obj.html().replace(/\n/g,'<br/>'));
+        }
+        $('#url').text(url);
+        $('#icon').attr("src", icon);
+        $('#worse').text(worse);
+        $('#moderate').text(moderate);
+        $('#better').text(better);
     });
 });
